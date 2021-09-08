16b575ca21b1391b6529f634703d8b7aa3bd9375
Matheus Cardoso <mhcardoso2001@gmail.com>
2021-09-08 12:52:18 -0300

    corrigido

--- Lab3/ex4.c
+++ Lab3/ex4.c
@@ -6,14 +6,14 @@ int main(void)
 	float max = FLT_MAX;
 	float min = FLT_MIN;
 
-	double dmax = LDBL_MAX;
-	double dmin = LDBL_MIN;
+	double dmax = DBL_MAX;
+	double dmin = DBL_MIN;
 
-	printf("%-20f %-5e %-5E\n", max, max, max);
-	printf("%-20f %-5e %-5E\n", min, min, min);
+	printf("%5f %-5e %-5E\n", max, max, max);
+	printf("%5f %-5e %-5E\n", min, min, min);
 
-	printf("%20f %-5e %-5E\n", dmax, dmax, dmax);
-	printf("%-20f %-5e %-5E\n", dmin, dmin, dmin);
+	printf("%5f %-5e %-5E\n", dmax, dmax, dmax);
+	printf("%5f %-5e %-5E\n", dmin, dmin, dmin);
 
 	return 0;
 
